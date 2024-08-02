4b46920bb1f9f073d50316a3c7d23793f986495b
Ashwini Tembhurne <ashwini101285@gmail.com>
2024-08-02 09:49:00 +0000

    create job application details view and template

--- job_application/views.py
+++ job_application/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import JobApplication
+from .forms import JobApplicationForm
 
-# Create your views here.
+def application_details(request, id):
+    application = get_object_or_404(JobApplication, id=id)
+    return render(request, 'job_application/application_details.html', {'application': application})
+
+def add_application(request):
+    if request.method == 'POST':
+        form = JobApplicationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = JobApplicationForm()
+    return render(request, 'job_application/application_form.html', {'form': form})
+
+def edit_application(request, id):
+    application = get_object_or_404(JobApplication, id=id)
+    if request.method == 'POST':
+        form = JobApplicationForm(request.POST, instance=application)
+        if form.is_valid():
+            form.save()
+            return redirect('application_details', id=id)
+    else:
+        form = JobApplicationForm(instance=application)
+    return render(request, 'job_application/application_form.html', {'form': form})
+
+def delete_application(request, id):
+    application = get_object_or_404(JobApplication, id=id)
+    if request.method == 'POST':
+        application.delete()
+        return redirect('home')
+    return render(request, 'job_application/confirm_delete.html', {'application': application})
