aadad2ef7bf253f24b8113b61325268a9cdd1493
Ashwini Tembhurne <ashwini101285@gmail.com>
2024-08-02 09:37:23 +0000

    Add routing for job application details page

---
+++ job_application/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('applications/<int:id>/', views.application_details, name='application_details'),
+    path('applications/new/', views.add_application, name='add_application'),
+    path('applications/edit/<int:id>/', views.edit_application, name='edit_application'),
+    path('applications/delete/<int:id>/', views.delete_application, name='delete_application'),
+]
