349df1fb1a1302face4f3b8559e489cd14c5675b
Ashwini Tembhurne <ashwini101285@gmail.com>
2024-08-06 13:20:58 +0000

    error in Job application STATUS is resolved

--- job_application/models.py
+++ job_application/models.py
@@ -11,12 +11,15 @@ class JobApplication(models.Model):
         ('OD', 'Offered'),
     ]
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=255)
     job_title = models.CharField(max_length=255)
     application_date = models.DateField()
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='', blank=True)
     notes = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.job_title
+
+    def get_status_display(self):
+        return dict(self.STATUS_CHOICES).get(self.status, 'Unknown Status')
