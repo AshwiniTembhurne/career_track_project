98c6d1f2770412706cafc251f4565a3e2fc91857
Ashwini Tembhurne <ashwini101285@gmail.com>
2024-08-06 15:01:58 +0000

    arrangement and position of all the user dashboard and admin dashboard fields is updated

--- admin_panel/admin.py
+++ admin_panel/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from job_application.models import JobApplication
 
-# Register your models here.
 @admin.register(JobApplication)
 class JobApplicationAdmin(admin.ModelAdmin):
-    list_display = ('job_title', 'company_name', 'status', 'application_date')
+    list_display = ('job_title', 'company_name', 'status', 'application_date', 'user')
     list_filter = ('status', 'application_date')
-    search_fields = ('job_title', 'company_name')
-
-# Register any other models you have
+    search_fields = ('job_title', 'company_name', 'user__username')
