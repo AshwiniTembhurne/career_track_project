e4f05528684553b5091625bd274514f7dbd7c4f4
Ashwini Tembhurne <ashwini101285@gmail.com>
2024-08-07 12:46:18 +0000

    error of logout message is fixed

--- user_accounts/views.py
+++ user_accounts/views.py
@@ -40,12 +40,10 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    messages.success(request, 'You have been logged out successfully.')
     return redirect('logout_success')
 
 def admin_logout_view(request):
     logout(request)
-    messages.success(request, 'Admin has been logged out successfully.')
     return redirect('logout_success')
 
 def logout_success(request):
